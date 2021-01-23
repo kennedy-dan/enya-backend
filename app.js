cc3c6bae142aec0115e01faadd7e267f3308da23
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2021-01-23 10:52:05 +0100

    first commit

--- app.js
+++ app.js
@@ -3,33 +3,30 @@ const app = express();
 const axios = require('axios');
 //const port = 3000;
 
-
-
-app.get ('/api/rates/', async(req, res) => {
+const enya = async(req, res) => {
 
     const {base, currency} = req.query
     try {
-        const results = await axios({
-            url: encodeURI(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ currency }`),
-            method: 'get',
-        })
+        const results = await axios.get(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ currency }`)
+
+        
 
         return res.status(200).send({
-            status_code: 200,
             results: results.data 
         });
           
     } catch (error) {
         return res.status(400).send({
-            status_code: 400,
             detail: error.message,
-            message: 'Invalid request'
+            message: 'request unsuccessful'
         })
     }
-})
+}
+
+app.get ('/api/rates/', enya)
 
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('server has started')
+    console.log('server has started on 3000')
 });
